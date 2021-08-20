02b4a165379606919385fa82e48e346aefc837f8
camielTR <89257910+camielTR@users.noreply.github.com>
2021-08-20 15:17:30 +0200

    Update NativeSettings.java

--- src/android/NativeSettings.java
+++ src/android/NativeSettings.java
@@ -170,12 +170,7 @@ public class NativeSettings extends CordovaPlugin {
         	return false;
         }
         
-        if(args.length() > 1 && args.getBoolean(1)) {
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        }
-	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+      	intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS | Intent.FLAG_ACTIVITY_REQUIRE_DEFAULT);
 
         this.cordova.getActivity().startActivity(intent);
         
